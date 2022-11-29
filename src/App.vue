fb3278823d0d04ceeae70d3089d7b3fd0a031e99
Renan Montano Paz <renanmontanopaz@outlook.com>
2022-11-29 11:58:44 -0300

    tela 1

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,15 @@
   <div id="app">
     <nav>
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/frete">Frete</router-link>
     </nav>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+@import "~bulma/bulma.sass";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/views/frete/FreteListView.vue
@@ -0,0 +1,200 @@
+<template>
+  <columns>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="column is-four-fifths">
+        <a class="navbar-item is-four-fifths" href="#">
+          <h1 class="title">Lista de Frete</h1>
+        </a>
+
+      </div>
+      <div class="column">
+        <button class="button is-success">Cadastrar</button>
+      </div>
+    </nav>
+
+    <div class="is-align-content-center">
+      <table class="table is-bordered">
+        <thead>
+        <tr class="is-selected">
+          <th><abbr title="Position">Data</abbr></th>
+          <th>Status do Frete</th>
+          <th><abbr title="Played">Cidade de Origem</abbr></th>
+          <th><abbr title="Won">Cidade de Destino</abbr></th>
+          <th><abbr title="Drawn">Placa do Caminhão</abbr></th>
+          <th><abbr title="Lost">Opção</abbr></th>
+        </tr>
+        </thead>
+        <tfoot>
+        <tr>
+          <th><abbr title="Position">Data</abbr></th>
+          <th>Status do Frete</th>
+          <th><abbr title="Played">Cidade de Origem</abbr></th>
+          <th><abbr title="Won">Cidade de Destino</abbr></th>
+          <th><abbr title="Drawn">Placa do Caminhão</abbr></th>
+          <th><abbr title="Lost">Opção</abbr></th>
+        </tr>
+        </tfoot>
+        <tbody>
+        <tr>
+          <th>11/11/2222</th>
+          <td><a href="#" title="Leicester City F.C.">Em Transporte</a> <strong>(C)</strong>
+          </td>
+          <td>Fod do Iguaçu</td>
+          <td>Toledo</td>
+          <td>BEW5E07</td>
+          <td>#</td>
+        </tr>
+        <tr>
+          <th>11/11/2222</th>
+          <td><a href="#" title="Leicester City F.C.">Em Transporte</a> <strong>(C)</strong>
+          </td>
+          <td>Fod do Iguaçu</td>
+          <td>Toledo</td>
+          <td>BEW5E07</td>
+          <td>#</td>
+        </tr>
+        <tr>
+          <th>11/11/2222</th>
+          <td><a href="#" title="Leicester City F.C.">Em Transporte</a> <strong>(C)</strong>
+          </td>
+          <td>Fod do Iguaçu</td>
+          <td>Toledo</td>
+          <td>BEW5E07</td>
+          <td>#</td>
+        </tr>
+        <tr>
+          <th>11/11/2222</th>
+          <td><a href="#" title="Leicester City F.C.">Em Transporte</a> <strong>(C)</strong>
+          </td>
+          <td>Fod do Iguaçu</td>
+          <td>Toledo</td>
+          <td>BEW5E07</td>
+          <td>#</td>
+        </tr>
+        <tr>
+          <th>11/11/2222</th>
+          <td><a href="#" title="Leicester City F.C.">Em Transporte</a> <strong>(C)</strong>
+          </td>
+          <td>Fod do Iguaçu</td>
+          <td>Toledo</td>
+          <td>BEW5E07</td>
+          <td>#</td>
+        </tr>
+        <tr>
+          <th>11/11/2222</th>
+          <td><a href="#" title="Leicester City F.C.">Em Transporte</a> <strong>(C)</strong>
+          </td>
+          <td>Fod do Iguaçu</td>
+          <td>Toledo</td>
+          <td>BEW5E07</td>
+          <td>#</td>
+        </tr>
+        <tr>
+          <th>11/11/2222</th>
+          <td><a href="#" title="Leicester City F.C.">Em Transporte</a> <strong>(C)</strong>
+          </td>
+          <td>Fod do Iguaçu</td>
+          <td>Toledo</td>
+          <td>BEW5E07</td>
+          <td>#</td>
+        </tr>
+        <tr>
+          <th>11/11/2222</th>
+          <td><a href="#" title="Leicester City F.C.">Em Transporte</a> <strong>(C)</strong>
+          </td>
+          <td>Fod do Iguaçu</td>
+          <td>Toledo</td>
+          <td>BEW5E07</td>
+          <td>#</td>
+        </tr>
+        <tr>
+          <th>11/11/2222</th>
+          <td><a href="#" title="Leicester City F.C.">Em Transporte</a> <strong>(C)</strong>
+          </td>
+          <td>Fod do Iguaçu</td>
+          <td>Toledo</td>
+          <td>BEW5E07</td>
+          <td>#</td>
+        </tr>
+        <tr>
+          <th>11/11/2222</th>
+          <td><a href="#" title="Leicester City F.C.">Em Transporte</a> <strong>(C)</strong>
+          </td>
+          <td>Fod do Iguaçu</td>
+          <td>Toledo</td>
+          <td>BEW5E07</td>
+          <td>#</td>
+        </tr>
+        <tr>
+          <th>11/11/2222</th>
+          <td><a href="#" title="Leicester City F.C.">Em Transporte</a> <strong>(C)</strong>
+          </td>
+          <td>Fod do Iguaçu</td>
+          <td>Toledo</td>
+          <td>BEW5E07</td>
+          <td>#</td>
+        </tr>
+        <tr>
+          <th>11/11/2222</th>
+          <td><a href="#" title="Leicester City F.C.">Em Transporte</a> <strong>(C)</strong>
+          </td>
+          <td>Fod do Iguaçu</td>
+          <td>Toledo</td>
+          <td>BEW5E07</td>
+          <td>#</td>
+        </tr>
+        <tr>
+          <th>11/11/2222</th>
+          <td><a href="#" title="Leicester City F.C.">Em Transporte</a> <strong>(C)</strong>
+          </td>
+          <td>Fod do Iguaçu</td>
+          <td>Toledo</td>
+          <td>BEW5E07</td>
+          <td>#</td>
+        </tr>
+        <tr>
+          <th>11/11/2222</th>
+          <td><a href="#" title="Leicester City F.C.">Em Transporte</a> <strong>(C)</strong>
+          </td>
+          <td>Fod do Iguaçu</td>
+          <td>Toledo</td>
+          <td>BEW5E07</td>
+          <td>#</td>
+        </tr>
+        <tr>
+          <th>11/11/2222</th>
+          <td><a href="#" title="Leicester City F.C.">Em Transporte</a> <strong>(C)</strong>
+          </td>
+          <td>Fod do Iguaçu</td>
+          <td>Toledo</td>
+          <td>BEW5E07</td>
+          <td>#</td>
+        </tr>
+        <tr>
+          <th>11/11/2222</th>
+          <td><a href="#" title="Leicester City F.C.">Em Transporte</a> <strong>(C)</strong>
+          </td>
+          <td>Fod do Iguaçu</td>
+          <td>Toledo</td>
+          <td>BEW5E07</td>
+          <td>#</td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+
+  </columns>
+
+</template>
+
+<script lang="ts">
+import {Component, Vue} from "vue-property-decorator";
+@Component
+export default class FreteListView extends Vue{
+
+}
+</script>
+
+<style scoped>
+
+</style>
